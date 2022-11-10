b63ac20e37e9f44a8660f811b0823075165ea965
dell <dell@DESKTOP-OFLO969>
2022-11-10 23:03:05 +0100

    Ajoutez des fichiers projet.

---
+++ ex3/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Point P1 = new Point();
+            Point P2 = new Point();
+            Point P3 = new Point();
+
+
+            Console.WriteLine("Entrez l'abscisse de 1ere point");
+            P1.Abs = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'ordonnee de 1ere point");
+            P1.Ord = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'abscisse de 2eme point");
+            P2.Abs = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'ordonnee de 2eme point");
+            P2.Ord = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'abscisse de 3eme point");
+            P3.Abs = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'ordonnee de 3eme point");
+            P3.Ord = int.Parse(Console.ReadLine());
+            //______Distance_____//
+            // Console.WriteLine("la distance entre ces deux points est :" + Distance(P1, P2));
+            //____Egalité____//
+            //Egalité(P1, P2);
+            //_____Translation___//
+            //Translation(P1);
+            //____Linéarité____//
+            // Linéarité(P1, P2, P3);
+        }
+    }
+}
---
+++ ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3
+{
+    internal class Program
+    {
+            static public void Linéarité(Point P1, Point P2, Point P3)
+            {
+                double a, b;
+                if (P1.Abs != P2.Abs) //on va montrer d'abord que 2 point sont alignees
+                {
+                    a = (P1.Ord - P2.Ord) / (P1.Abs - P2.Abs);
+                    b = P1.Ord + a * P1.Abs;   //determiner l'equation de la droite y=ax+b
+                    if (a * P3.Abs + b == P3.Ord)//verifier l'appartenence du 3eme point a la droite (P1P2) 
+                    {
+                        Console.WriteLine("les 3 points sont lineaire");
+                    }
+                    else
+                        Console.WriteLine("les 3 points ne sont pas lineaire");
+                }
+            }
+    }
+}
